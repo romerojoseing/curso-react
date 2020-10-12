0bef1f66a6e6eb55f336f6164e7b4e6565307921
romerojoseing <romerojoseing@gmail.com>
2020-10-12 15:01:18 -0400

    New

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState, Fragment} from 'react';
 
 const Contador = () => {
-  return ( 
-    <h3>Componente Contador</h3>
-   );
+
+  const [numero, setNumero] = useState(0);
+
+  const disminuir = () => {
+    setNumero(numero - 1)
+  }
+
+  return (
+    <Fragment>
+      <h3>Componente Contador {numero} </h3>
+      <button onClick={() => setNumero(numero + 1)}>Aumentar</button>     {/* Se puede hacer de las dos maneras Aumentar o Dismnuir*/}
+      <button onClick={disminuir}>Disminuir</button>
+    </Fragment>
+  );
 }
  
 export default Contador;
